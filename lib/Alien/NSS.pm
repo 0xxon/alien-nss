81babf521dc3cffafc8286eb1bce6efb18d5c959
Johanna Amann <johanna@icir.org>
2012-12-06 02:21:14 -0800

    first version, which already kind of seems to work. We will see...

---
+++ lib/Alien/NSS.pm
@@ -0,0 +1,26 @@
+package Alien::NSS;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Acme::NSS - Alien wrapper for NSS ( Network Security Services )
+
+=head1 ABSTRACT
+
+Alien wrapper for NSS ( Network Security Services )
+
+=head1 DESCRIPTION
+
+This library provides an alien wrapper for NSS, the cryptographic
+library that is ( among others ) used in Mozilla Firefox and Google Chrome.
+
